adbc721bfd7a2ff8df746c1c8537cc36f317e54b
rohannj1993 <rohannj1993@gmail.com>
2021-09-11 12:40:00 -0400

    updated some of the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import gitlogo from "./Assets/GitHub-Mark/PNG/GitHub-Mark-Light-64px.png"
+import gitlogo from "./Assets/GitHub-Mark/PNG/GitHub-Mark-Light-64px.png";
+import linkedinlogo from "./Assets/LinkedIn-Logos/LI-Logo.png"
 function Footer(){
     return(
 <p>
+    <a href='#'>
+
+<img src ={linkedinlogo} id='linkedin' alt="linkedin logo"/>
+    </a>
+    <a href='#'>
 
  <img src ={gitlogo} alt="github logo"/>
+    </a>
 </p>
     )
 }
