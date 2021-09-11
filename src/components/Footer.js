b779deca5de3934d4f6efb942bec60b140bfd41e
rohannj1993 <rohannj1993@gmail.com>
2021-09-11 13:26:50 -0400

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import gitlogo from "./Assets/GitHub-Mark/PNG/GitHub-Mark-Light-64px.png";
-import linkedinlogo from "./Assets/LinkedIn-Logos/LI-Logo.png"
+import linkedinlogo from "./Assets/LinkedIn-Logos/LI-Logo.png";
+import stacklogo from "./Assets/Stackoverflowlogo.png"
 function Footer(){
     return(
 <p>
-    <a href='#'>
+    <a href='https://www.linkedin.com/in/rohan-nair-23b87121b/'>
 
 <img src ={linkedinlogo} id='linkedin' alt="linkedin logo"/>
     </a>
-    <a href='#'>
+    <a href='https://github.com/rohannj1993'>
 
  <img src ={gitlogo} alt="github logo"/>
     </a>
+    
 </p>
     )
 }
